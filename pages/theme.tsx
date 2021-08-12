aaae2aa4766d823c27382c0b4143fb1893c1a5cd
Gabriel Messias <gabrielmessiasdarosa@gmail.com>
2021-08-12 09:59:47 -0300

    FIX: Fix build vercel error, remove export default

--- pages/theme.tsx
+++ pages/theme.tsx
@@ -2,16 +2,16 @@
 import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` };
+export const fonts = { mono: `'Menlo', monospace` };
 
-const breakpoints = createBreakpoints({
+export const breakpoints = createBreakpoints({
   sm: "40em",
   md: "52em",
   lg: "64em",
   xl: "80em",
 });
 
-const theme = extendTheme({
+export const theme = extendTheme({
   colors: {
     black: "#16161D",
   },
@@ -19,4 +19,4 @@ const theme = extendTheme({
   breakpoints,
 });
 
-export default theme;
+
